19d9252183989a7fb0ca7764d5b838dbf856f8b8
GermanViter <german.viter@icloud.com>
2025-12-21 01:31:38 +0000

    feat: add initial backup script v1

--- save.sh
+++ save.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+SOURCE="/tmp/sources_text"
+DEST="/tmp/sauvegardes"
 
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_DIR="$DEST/backup_$DATE"
+
+if [[ ! -d "$SOURCE" ]]; then
+	echo "Erreur: le dossier source n'existe pas"
+	exit 1
+fi
+
+mkdir -p "$BACKUP_DIR"
+
+cp -r "$SOURCE/" "$BACKUP_DIR"
+
+echo "sauvegarde terminee avec succes : $BACKUP_DIR"
